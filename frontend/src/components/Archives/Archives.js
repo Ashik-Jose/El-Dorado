0d969d58886de6fd6e5ca1f236a53927b81402b8
Ashik-Jose <ashiksholly@gmail.com>
2022-05-14 18:41:33 +0530

    Ashik

--- frontend/src/components/Archives/Archives.js
+++ frontend/src/components/Archives/Archives.js
@@ -1,12 +1,15 @@
+import './Archives.css'
 
-import './Archives.css';
-function Archives()
-{
-    return(
-        <div className='Archives'>
-            <h1>Sugathakumari</h1>
+const Archives = () => {
+    return (
+        <div>
+            <div className="four">
+                <h1 className="hname">KUNJAN NAMBIAR</h1><br />
+                <img src="kunjannambiar.png" className="kunjannambiar" /><br /><br />
+                <h3>HISTORY</h3>
+                <p>Kalakkathu Kunchan Nambiar (Malayalam: കുഞ്ചൻ നമ്പ്യാർ, romanized: kuñcan Nambiār) was an early Malayalam poet, performer, satirist and the inventor of local art form of Ottamthullal.Nambiar is believed to have been born at Kalathu Veedu at Killikkurussimangalam in Palakkad district of the south Indian state of Kerala; He spent his early childhood at Killikkurussimangalam, his boyhood at Kudamaloor and youth at Ambalappuzha, and learnt Kalaripayattu and Sanskrit from such masters as Mathoor Panickar, Dronaballi Naicker and Nannikod Unni Ravi Kurup, before moving to the court of Marthanda Varma of Travancore in 1748; later, he served at the court of his successor Dharma Raja. By the time he reached the royal court, he had already established himself as a poet. The later part of his life, it is believed that Nambiar returned to Ambalapuzha where he died in 1770, at the age of 65, reportedly due to rabies.</p>
+                <iframe width={683} height={384} src="https://www.youtube.com/embed/Ya7jnIs8xwY" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /> --&gt; --&gt;
+            </div>
         </div>
     );
-} 
-
-export default Archives;
+}
